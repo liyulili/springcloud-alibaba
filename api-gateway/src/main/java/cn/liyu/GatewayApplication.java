3cea4e4fbd21c169c4790b4f8cf41ff43516c1d4
liyu <1170667320@qq.com>
2020-04-28 16:45:47 +0800

    创建网关工程

---
+++ api-gateway/src/main/java/cn/liyu/GatewayApplication.java
@@ -0,0 +1,18 @@
+package cn.liyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author liyu
+ * @date 2020/4/28 15:59
+ * @description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
