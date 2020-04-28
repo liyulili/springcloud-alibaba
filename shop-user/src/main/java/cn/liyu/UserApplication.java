cf06cd345c7c12fb473c6e15116fce0b47f8d945
liyu <1170667320@qq.com>
2020-04-28 14:15:48 +0800

    创建user工程

---
+++ shop-user/src/main/java/cn/liyu/UserApplication.java
@@ -0,0 +1,17 @@
+package cn.liyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liyu
+ * @date 2020/4/27 15:24
+ * @description
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
